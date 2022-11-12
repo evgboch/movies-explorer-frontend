21b310befea1feb65c2f93fd18ab32d9a69f4f1e
Evgeny Bocharov <evgbocharov@gmail.com>
2022-11-13 02:50:20 +0300

    fix: сделать лого в хедере страниц логина и регистрации ссылкой

--- src/components/EntryHeader/EntryHeader.js
+++ src/components/EntryHeader/EntryHeader.js
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom";
 import "./EntryHeader.css";
+
 
 function EntryHeader({ children }) {
   return (
     <header className="entry-header">
-      <div className="entry-header__logo"></div>
+      <Link to="/" className="entry-header__logo"></Link>
       <h2 className="entry-header__greeting">{ children }</h2>
     </header>
   )
