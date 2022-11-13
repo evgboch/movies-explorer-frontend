a695548977c289c991c170b9a625872fc4dc5b3b
Evgeny Bocharov <evgbocharov@gmail.com>
2022-11-13 18:18:54 +0300

    fix: исправить ошибки/предупреждения в консоли

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -11,13 +11,13 @@ function Login() {
       <form className="login__form" onSubmit={ handleSubmit }>
         <label className="login__form-label" htmlFor="login-email">
           E-mail
-          <input className="login__form-input" id="login-email" required="true"></input>
-          <span class="login__form-error"></span>
+          <input className="login__form-input" id="login-email" required={ true }></input>
+          <span className="login__form-error"></span>
         </label>
         <label className="login__form-label" htmlFor="login-password">
           Пароль
-          <input className="login__form-input" id="login-password" type="password" required="true"></input>
-          <span class="login__form-error"></span>
+          <input className="login__form-input" id="login-password" type="password" autoComplete="on" required={ true }></input>
+          <span className="login__form-error"></span>
         </label>
         <button className="login__submit" type="submit">Войти</button>
       </form>
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -11,18 +11,18 @@ function Register() {
       <form className="register__form" onSubmit={ handleSubmit }>
         <label className="register__form-label" htmlFor="register-name">
           Имя
-          <input className="register__form-input" id="register-name" required="true"></input>
-          <span class="register__form-error"></span>
+          <input className="register__form-input" id="register-name" required={ true }></input>
+          <span className="register__form-error"></span>
         </label>
         <label className="register__form-label" htmlFor="register-email">
           E-mail
-          <input className="register__form-input" id="register-email" required="true"></input>
-          <span class="register__form-error"></span>
+          <input className="register__form-input" id="register-email" required={ true }></input>
+          <span className="register__form-error"></span>
         </label>
         <label className="register__form-label " htmlFor="register-password">
           Пароль
-          <input className="register__form-input register__form-input_error" id="register-password" type="password" required="true"></input>
-          <span class="register__form-error register__form-error_visible">Что-то пошло не так...</span>
+          <input className="register__form-input register__form-input_error" id="register-password" type="password" autoComplete="off" required={ true }></input>
+          <span className="register__form-error register__form-error_visible">Что-то пошло не так...</span>
         </label>
         <button className="register__submit" type="submit">Зарегистрироваться</button>
       </form>
